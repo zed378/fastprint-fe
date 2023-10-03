a6187e6db8d25d3640381f599a221073370ec920
zed378 <kembedt@gmail.com>
2023-10-03 18:26:25 +0700

    change API URI

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    // API_URI: "https://frail-calf-sari.cyclic.cloud/api/v1",
-    API_URI: "http://localhost:5000/api/v1",
+    API_URI: "https://frail-calf-sari.cyclic.cloud/api/v1",
   },
 };
 
